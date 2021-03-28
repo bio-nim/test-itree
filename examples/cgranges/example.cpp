59d1a0df59bbd59a8ce9a1d8a6d0bbed2e974dfc
Christopher Dunn <cdunn@pacificbiosciences.com>
2021-03-28 10:51:34 -0500

    Try (2,2) -- odd!

--- examples/cgranges/example.cpp
+++ examples/cgranges/example.cpp
@@ -1,15 +1,37 @@
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
 #include "IITree.h"
+using namespace std;
 
+using IntTree = IITree<int, int>;
+
+void Dump(IntTree const& it)
+{
+    for (size_t j = 0; j < it.size(); ++j) {
+        printf("\t%zu\t%d\t%d\t%d\n",
+                j, it.start(j), it.end(j), it.data(j));
+    }
+}
 int main(void)
 {
+    int n = 0;
 	IITree<int, int> tree;
-	tree.add(12, 34, 0);
-	tree.add(0, 23, 1);
-	tree.add(34, 56, 2);
+	tree.add(12, 34, n++);
+	tree.add(0, 23, n++);
+	tree.add(34, 56, n++);
+	tree.add(21, 22, n++);
+	tree.add(22, 22, n++);
+	tree.add(22, 23, n++);
+	tree.add(25, 25, n++);
+	tree.add(25, 26, n++);
 	tree.index();
+    cout << "All:\n";
+    Dump(tree);
+    int left = 22;
+    int right = 22;
+    cout << "overlap(" << left << ", " << right << ")\n";
 	std::vector<size_t> a;
-	tree.overlap(22, 25, a);
+	tree.overlap(left, right, a);
 	for (size_t i = 0; i < a.size(); ++i)
 		printf("%d\t%d\t%d\n", tree.start(a[i]), tree.end(a[i]), tree.data(a[i]));
 	return 0;
