47720290af9624c66c8824a2d70602a8c21a2268
Christopher Dunn <cdunn@pacificbiosciences.com>
2021-04-07 12:03:49 -0500

    Use hengli tabs

--- examples/cgranges/example.cpp
+++ examples/cgranges/example.cpp
@@ -1,3 +1,4 @@
+// vim: sw=4 ts=4 sts=4 tw=0 noet:
 #include <cstdio>
 #include <iostream>
 #include "IITree.h"
@@ -7,15 +8,15 @@ using IntTree = IITree<int, int>;
 
 void Dump(IntTree const& it)
 {
-    for (size_t j = 0; j < it.size(); ++j) {
-        printf("\t%zu\t%d\t%d\t%d\n",
-                j, it.start(j), it.end(j), it.data(j));
-    }
+	for (size_t j = 0; j < it.size(); ++j) {
+		printf("\t%zu\t%d\t%d\t%d\n",
+			j, it.start(j), it.end(j), it.data(j));
+	}
 }
 int main(void)
 {
-    int n = 0;
 	IITree<int, int> tree;
+	int n = 0;
 	tree.add(12, 34, n++);
 	tree.add(0, 23, n++);
 	tree.add(34, 56, n++);
@@ -25,11 +26,11 @@ int main(void)
 	//tree.add(25, 25, n++);
 	//tree.add(25, 26, n++);
 	tree.index();
-    cout << "All:\n";
-    Dump(tree);
-    int left = 22;
-    int right = 23;
-    cout << "overlap(" << left << ", " << right << ")\n";
+	cout << "All:\n";
+	Dump(tree);
+	int left = 22;
+	int right = 23;
+	cout << "overlap(" << left << ", " << right << ")\n";
 	std::vector<size_t> a;
 	tree.overlap(left, right, a);
 	for (size_t i = 0; i < a.size(); ++i)
--- examples/intervaltree/example.cpp
+++ examples/intervaltree/example.cpp
@@ -13,24 +13,21 @@ int main(void)
     int n = 100;
     IntervalTree<std::size_t, int> t{
         {
-            {1,3, n++},
-            {1,3, n++},
-            {0, 10, n++},
-            {8, 10, n++},
-            /*
             {12, 35, n++},
             {0, 24, n++},
             {34, 57, n++},
             {21, 23, n++},
             {22, 23, n++},
             {22, 24, n++},
-            */
         },
     4, 64, 512};
     cout << "All:\n";
     cout << t;
     cout << "?END.\n";
-	auto v = t.findOverlapping(1,1);
+    int left = 23;
+    int right = 23;
+    cout << "overlap(" << left << ", " << right << ")\n";
+	auto v = t.findOverlapping(left, right);
     cout << "Found:\n";
     for (auto x : v) {
         cout << x << "\n";
